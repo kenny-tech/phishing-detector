4c3c0d4ecfaabfcdc9d98d89848c5422a212262c
Kehinde Adegbesan <kehindeadegbesan@duronetworks.com>
2023-06-27 13:48:52 +0100

    Updated about view to display well on dark mode

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, useColorScheme } from 'react-native';
 
 const AboutScreen = () => {
+
+    const colorScheme = useColorScheme();
+    const isDarkMode = colorScheme === 'dark';
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.content}>This application warns you about spam and unwanted messages. It does this by analyzing the content of the message and using machine learning algorithms to detect patterns.</Text>
+        <View style={[styles.container, { backgroundColor: isDarkMode ? '#000000' : '#F5F5F5' }]}>
+            <Text style={[styles.message, { color: isDarkMode ? '#FFFFFF' : '#282828' }]}>This app warns you about spam and unwanted messages. It does this by analyzing the content of the message and using machine learning algorithms to detect patterns.</Text>
         </View>
     )
 }
@@ -15,11 +19,12 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         alignItems: 'center',
     },
-    content: {
-        padding: 8,
-        color: '#282828'
+    message: {
+        fontSize: 16,
+        color: '#282828',
+        padding: 20,
     },
-   
+
 });
 
 export default AboutScreen
